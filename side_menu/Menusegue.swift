cecf257c06b0477cf1d8f00b361c4715a0c145b2
Takatoshi Watanabe <abacus.taka1125.wtnbtkts@gmail.com>
2015-10-31 11:51:20 +0900

    first commit

---
+++ side_menu/Menusegue.swift
@@ -0,0 +1,9 @@
+//
+//  Menusegue.swift
+//  side_menu
+//
+//  Created by Takatoshi Watanabe on 2015/10/31.
+//  Copyright © 2015年 Takatoshi Watanabe. All rights reserved.
+//
+
+import Foundation
---
+++ side_menu/sidemenu.h
@@ -0,0 +1,13 @@
+//
+//  sidemenu.h
+//  side_menu
+//
+//  Created by Takatoshi Watanabe on 2015/10/31.
+//  Copyright © 2015年 Takatoshi Watanabe. All rights reserved.
+//
+
+#ifndef sidemenu_h
+#define sidemenu_h
+
+
+#endif /* sidemenu_h */
